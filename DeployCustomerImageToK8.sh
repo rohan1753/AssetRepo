f6412370797c31cc272398cd5f7365176460275e
meerarfath <meer.arfath@softwareag.com>
2021-02-23 17:59:40 +0530

    Update DeployCustomerImageToK8.sh

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,5 @@
 #
 
 cd /opt/rancher
-./rancher login https://k8s.eur.ad.sag/v3 --token token-kw28t:nqcsr5k48hpqk2wpxfjtg8gh5zc79vhzhd4lq6hgjtm9khstggtz54
+./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
